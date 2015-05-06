58836d385d122fad9010f00958732715eb2e7d53
Stefan Carlton <me@stefancarlton.net>
2015-05-06 16:14:52 +1000

    Make the wiring conversion easier to understand by using an array

--- wordclock_tools/wiring.py
+++ wordclock_tools/wiring.py
@@ -16,7 +16,7 @@ class wiring:
         stancil_content  = ast.literal_eval(config.get('language_options', language))
         self.WCA_HEIGHT  = len(stancil_content)
         self.WCA_WIDTH   = len(stancil_content[0].decode('utf-8'))
-        self.LED_COUNT   = self.WCA_WIDTH*self.WCA_HEIGHT+4 # Number of LED pixels.
+        self.LED_COUNT   = self.WCA_WIDTH*self.WCA_HEIGHT # Number of LED pixels.
         self.LED_PIN     = 18      # GPIO pin connected to the pixels (must support PWM!).
         self.LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
         self.LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
@@ -66,6 +66,20 @@ class english_wiring:
     def __init__(self, WCA_WIDTH, WCA_HEIGHT):
         self.WCA_WIDTH   = WCA_WIDTH
         self.WCA_HEIGHT  = WCA_HEIGHT
+        
+        selft.WCA_GRID = [[0 for x in range(WCA_HEIGHT)] for x in range(self.WCA_WIDTH)] 
+        self.WCA_GRID[0]=[99, 98, 77, 76, 55, 54, 33, 32, 11, 10]
+        self.WCA_GRID[1]=[100, 97, 78, 75, 56, 53, 34, 31, 12, 9]
+        self.WCA_GRID[2]=[101, 96, 79, 74, 57, 52, 35, 30, 13, 8]
+        self.WCA_GRID[3]=[102, 95, 80, 73, 58, 51, 36, 29, 14, 7]
+        self.WCA_GRID[4]=[103, 94, 81, 72, 59, 50, 37, 28, 15, 6]
+        self.WCA_GRID[5]=[104, 93, 82, 71, 60, 49, 38, 27, 16, 5]
+        self.WCA_GRID[6]=[105, 92, 83, 70, 61, 48, 39, 26, 17, 4]
+        self.WCA_GRID[7]=[106, 91, 84, 69, 62, 47, 40, 25, 18, 3]
+        self.WCA_GRID[8]=[107, 90, 85, 68, 63, 46, 41, 24, 19, 2]
+        self.WCA_GRID[9]=[108, 89, 86, 67, 64, 45, 42, 23, 20, 1]
+        self.WCA_GRID[10]=[109, 88, 87, 66, 65, 44, 43, 22, 21, 0]
+
 
     def getStripIndexFrom2D(self, x, y):
         '''
@@ -75,11 +89,7 @@ class english_wiring:
              (0,0): top-left
              (self.WCA_WIDTH-1, self.WCA_HEIGHT-1): bottom-right
         '''
-        if x%2 == 0:
-            pos = (self.WCA_WIDTH-x-1)*self.WCA_HEIGHT+y+2
-        else:
-            pos = (self.WCA_WIDTH*self.WCA_HEIGHT)-(self.WCA_HEIGHT*x)-y+1
-        return pos
+        return self.WCA_GRID[y,x]
 
     def mapMinutes(self, min):
         '''
@@ -87,15 +97,16 @@ class english_wiring:
         Needs hardware/wiring dependent implementation
         This implementation assumes the minutes to be wired as first and last two leds of the led-strip
         '''
+        
         if min == 1:
-            return 100
+            return self.getStripIndexFrom2D(self, 10,0)
         elif min == 2:
-            return 101
+            return self.getStripIndexFrom2D(self, 10,1)
         elif min == 3:
-            return 108
+            return self.getStripIndexFrom2D(self, 10,8)
         elif min == 4:
-            return 109
+            return self.getStripIndexFrom2D(self, 10,9)
         else:
             print('WARNING: Out of range, when mapping minutes...')
             print(min)
-            return 0
+            return self.getStripIndexFrom2D(self, 10,0)
